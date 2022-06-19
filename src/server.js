34c58528e01995d95f06421fccc00ee5b86fff19
hyejeongjang <hj970219@naver.com>
2022-06-19 23:59:02 +0900

    solve problem called Unexpected token '<'

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app=express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname+"/views");
-app.use("/public", express.static(__dirname="/public")); // 뷰에서 public 경로로 찾아갈 수 있음
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
